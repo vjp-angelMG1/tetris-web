bae7575a1cb0a0e128762b527da2b491bd3513be
vjp-angelMG1 <amonterog04@informatica.iesvalledeljerteplasencia.es>
2026-05-21 09:25:58 +0200

    cambios

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -17,4 +17,4 @@ const firebaseConfig = {
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
-export const db = getFirestore(app);
+export const db = getFirestore(app); 
